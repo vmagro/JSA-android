7144d20bb6d2cb916a2f675bdf03e32433914890
vmagro <vmagro@socaldevs.com>
2013-01-12 09:08:57 -0800

    start using simplesectionadapter

--- src/org/jsa/socal/mobile/android/api/Api.java
+++ src/org/jsa/socal/mobile/android/api/Api.java
@@ -2,7 +2,8 @@ package org.jsa.socal.mobile.android.api;
 
 public class Api {
 	
-	public static final String BASE = "http://socal-jsa.appspot.com/api/";
+	//public static final String BASE = "http://socal-jsa.appspot.com/api/";
+	public static final String BASE = "http://192.168.0.9:8888/api/";
 	
 	public static final String CONVENTIONS		= BASE + "conventions";
 	
--- src/org/jsa/socal/mobile/android/fragments/ConventionFragment.java
+++ src/org/jsa/socal/mobile/android/fragments/ConventionFragment.java
@@ -2,55 +2,58 @@ package org.jsa.socal.mobile.android.fragments;
 
 import java.util.ArrayList;
 
-import org.jsa.socal.mobile.android.api.Convention;
 import org.jsa.socal.mobile.android.api.AgendaTopic;
+import org.jsa.socal.mobile.android.api.Convention;
 import org.socal.jsa.mobile.android.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.mobsandgeeks.adapters.Sectionizer;
+import com.mobsandgeeks.adapters.SimpleSectionAdapter;
 
-public class ConventionFragment extends SherlockFragment implements
-		OnItemClickListener {
+public class ConventionFragment extends SherlockListFragment{
 
-	private AgendaAdapter adapter;
+	private SimpleSectionAdapter<AgendaTopic> adapter;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup root,
-			Bundle savedInstanceState) {
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
 		Convention c = (Convention) getArguments()
 				.getSerializable("convention");
+		
+		Log.i("convention", c.getName());
 
 		getSherlockActivity().getSupportActionBar().setTitle(c.getName());
 
-		View v = inflater.inflate(R.layout.convention, null);
-		((TextView) v.findViewById(R.id.title)).setText(c.getName());
-		((TextView) v.findViewById(R.id.date)).setText(c.getDateString());
-		((TextView) v.findViewById(R.id.location)).setText(c.getLocation());
-
-		if (c.getAgenda().size() == 0) {
-			((LinearLayout) v).removeView(v.findViewById(R.id.agendaText));
-		} else
-			((TextView) v.findViewById(R.id.agendaText)).setText(Html
-					.fromHtml(getString(R.string.agenda_u)));
-
-		ListView lv = (ListView) v.findViewById(R.id.agenda);
-		adapter = new AgendaAdapter(getActivity().getLayoutInflater(), c);
-		lv.setAdapter(adapter);
-		lv.setOnItemClickListener(this);
-
-		return v;
+		adapter = new SimpleSectionAdapter<AgendaTopic>(getActivity(), 
+				new AgendaAdapter(getActivity().getLayoutInflater(), c), R.layout.section_header, R.id.title, 
+		        new AgendaSectionizer());
+		
+		this.setListAdapter(adapter);
+	}
+	
+	@Override
+	public void onListItemClick(ListView lv, View v, int position, long id) {
+		AgendaTopic t = (AgendaTopic) adapter.getItem(position);
+		Bundle args = new Bundle();
+		args.putSerializable("topic", t);
+		Fragment frag = Fragment.instantiate(getActivity(),
+				AgendaTopicFragment.class.getName(), args);
+		getSherlockActivity().getSupportFragmentManager().beginTransaction()
+				.detach(this).add(android.R.id.content, frag)
+				.addToBackStack("debate").commit();
 	}
 
 	private class AgendaAdapter extends BaseAdapter {
@@ -58,7 +61,7 @@ public class ConventionFragment extends SherlockFragment implements
 		private LayoutInflater inflater;
 		private ArrayList<AgendaTopic> agenda;
 
-		private AgendaAdapter(LayoutInflater inflater, Convention c) {
+		private AgendaAdapter(LayoutInflater inflater, Convention c){
 			this.inflater = inflater;
 			this.agenda = c.getAgenda();
 		}
@@ -81,29 +84,35 @@ public class ConventionFragment extends SherlockFragment implements
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View v = inflater.inflate(R.layout.agenda_list_item, null);
-			((TextView) v.findViewById(R.id.block)).setText(Html
-					.fromHtml(agenda.get(position).getBlock()));
+			
+			if(agenda.size() <= position)
+				return v;
+			
 			((TextView) v.findViewById(R.id.text)).setText(Html.fromHtml(agenda
 					.get(position).getText()));
-			((TextView) v.findViewById(R.id.time)).setText(Html.fromHtml(agenda
-					.get(position).getTime()));
+			
+			if(agenda.get(position).getText().equals(""))
+				((LinearLayout) v).removeView(v.findViewById(R.id.text)); //remove the view for description if there is no description
+			
 			((TextView) v.findViewById(R.id.location)).setText(Html.fromHtml(agenda
 					.get(position).getLocation()));
 			return v;
 		}
 
 	}
+	
+	private static class AgendaSectionizer implements Sectionizer<AgendaTopic>{
 
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View v, int pos, long id) {
-		AgendaTopic t = adapter.getItem(pos);
-		Bundle args = new Bundle();
-		args.putSerializable("topic", t);
-		Fragment frag = Fragment.instantiate(getActivity(),
-				AgendaTopicFragment.class.getName(), args);
-		getSherlockActivity().getSupportFragmentManager().beginTransaction()
-				.detach(this).add(android.R.id.content, frag)
-				.addToBackStack("debate").commit();
+		@Override
+		public String getSectionTitleForItem(AgendaTopic topic) {
+			return topic.getBlock();
+		}
+		
+		@Override
+		public String getTime(AgendaTopic topic){
+			return topic.getTime();
+		}
+		
 	}
 
 }
